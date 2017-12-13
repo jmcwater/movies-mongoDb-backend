eab02f2f3260b5f31a59e6a863dd69abce781289
jordanh2os <jmc.waters@student.avans.nl>
2017-12-14 00:53:01 +0100

    heroku fix?

--- src/model/movie.model.js
+++ src/model/movie.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const TheatreSchema = require('./theatre.schema');
+const TheatreSchema = require('./Theatre.schema');
 const MoviesPlayingSchema = require('./moviesPlaying.schema');
 const MovieSchema = require('./movie.schema');
 const Schema = mongoose.Schema;
